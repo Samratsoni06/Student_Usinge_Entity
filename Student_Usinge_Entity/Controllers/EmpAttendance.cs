b04d8e4ebc70c3dbd5e8638f34ca9abefbc1cd56
Samratsoni06 <samratsoni800@gmail.com>
2024-09-25 12:37:16 +0530

    Add in Model

--- Student_Usinge_Entity/Controllers/EmpAttendance.cs
+++ Student_Usinge_Entity/Controllers/EmpAttendance.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public int UserID { get; set; }
+        public int EmployeeID { get; set; }
+        public string EmpName { get; set; } = "";
         public string CurrentLocation { get; set; } = "";
         public DateTime? CheekIN { get; set; }
         public DateTime? CheekOUT { get; set; }
