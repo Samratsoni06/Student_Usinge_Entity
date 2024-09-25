4a8dc901d13a9e75c1215f58d95c94e2a6e30836
Samratsoni06 <samratsoni800@gmail.com>
2024-09-25 12:33:34 +0530

    Done

---
+++ Student_Usinge_Entity/Controllers/EmpAttendance.cs
@@ -0,0 +1,12 @@
+﻿namespace Student_Usinge_Entity.Controllers
+{
+    public class EmpAttendance
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+        public string CurrentLocation { get; set; } = "";
+        public DateTime? CheekIN { get; set; }
+        public DateTime? CheekOUT { get; set; }
+        public string TimeZone { get; set; } = "";
+    }
+}
